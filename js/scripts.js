c617b28165594f2006712921de4c8a0ec08b1df4
123mwinslow <71267554+123mwinslow@users.noreply.github.com>
2021-10-28 10:28:52 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -54,3 +54,39 @@ window.addEventListener("DOMContentLoaded", (event) => {
     elements: "#portfolio a.portfolio-box"
   });
 });
+  <div class="container">
+      <h4 class="m-4">Write a function to make this page functional</h4>
+
+      <div class="row text-center">
+        <div class="col-3 m-auto">
+          <input type="number" id="inputNum1" class="form-control" />
+        </div>
+        <div class="col-2 m-auto">
+          <h2>X</h2>
+        </div>
+        <div class="col-3 m-auto">
+          <input type="number" id="inputNum2" class="form-control" />
+        </div>
+        <div class="col-2 m-auto">
+          <button
+            type="button"
+            class="btn btn-outline-dark"
+            onclick="multiplyNumbers()"
+          >
+            <h2>=</h2>
+          </button>
+        </div>
+        <div class="col-2">
+          <h2 id="multiplyAnswer">?</h2>
+        </div>
+      </div>
+    </div>
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
+      crossorigin="anonymous"
+    ></script>
+
+    <script src="index.js"></script>
